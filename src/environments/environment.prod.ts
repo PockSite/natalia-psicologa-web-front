e14f080fff73c4aa2f3e1d89dbae7f434e948410
Spdrd <juandiegoreyesrodriguez@gmail.com>
2026-07-08 15:29:43 -0500

    add envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  productsApiUrl: 'https://devnataliaproductsapi.pocksite.com/api/v1',
+  reservationsApiUrl: 'https://devnataliareservationssapi.pocksite.com/api/v1',
+  notificationsApiUrl: 'https://devnatalianotificationsapi.pocksite.com/api/v1',
+  clientsApiUrl: 'https://devnataliaclientsapi.pocksite.com/api/v1',
+  paymentsApiUrl: 'https://devnataliapayapi.pocksite.com/api/v1',
+};
